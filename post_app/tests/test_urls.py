b5ed61ddbda866a4711658012ac0d6a020ef88fd
Mori <morteza.fazel@gmail.com>
2023-07-10 16:03:27 +0200

    test_urls works properly

--- post_app/tests/test_urls.py
+++ post_app/tests/test_urls.py
@@ -1,21 +1,18 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from post_app.views import test_view, create_experience, experience_list, experience_detail, post_list, post_create, post_detail, post_update  
+from post_app.views import test_view, create_experience, experience_list, experience_detail  
+
 
 class TestUrls(SimpleTestCase):
 
     def test_list_url_is_resolved(self):
         url = reverse('post_app:post_list')
-        self.assertEquals(resolve(url).func, post_list)
+        self.assertEquals(resolve(url).func, experience_list)
 
     def test_create_url_is_resolved(self):
         url = reverse('post_app:post_create')
-        self.assertEquals(resolve(url).func, post_create)
+        self.assertEquals(resolve(url).func, create_experience)
 
     def test_detail_url_is_resolved(self):
         url = reverse('post_app:post_detail', args=[1])
-        self.assertEquals(resolve(url).func, post_detail)
-
-    def test_update_url_is_resolved(self):
-        url = reverse('post_app:post_update', args=[1])
-        self.assertEquals(resolve(url).func, post_update)
+        self.assertEquals(resolve(url).func, experience_detail)
