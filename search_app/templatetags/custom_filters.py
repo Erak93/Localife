50b20c8926745f28cb09bc85e9fea556125f8f63
Pepper <pepperoncanvas@gmail.com>
2023-08-08 03:02:59 +0200

    user profileupdate

---
+++ search_app/templatetags/custom_filters.py
@@ -0,0 +1,15 @@
+from django import template
+register = template.Library()
+@register.filter
+def get_star_emoji(rating):
+    return ":star:️" * rating
+@register.filter
+def extract_id(name):
+    from user_app.models import Experience
+    experience = Experience.objects.get(title=name)
+    return experience.id
+@register.filter
+def extract_host(host_name):
+    from user_app.models import Experience
+    experience = Experience.objects.get(host__user__username=host_name)
+    return experience.id
